c1f11bdb366f10b42cb8ff0deb8ae5eb2d83579a
Imad Aziz <58164265+ImadAzizKhan@users.noreply.github.com>
2021-11-07 23:38:28 +0500

    Create Code 2.ino

---
+++ Code 2.ino
@@ -0,0 +1,182 @@
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com
+  Arduino IDE example: Examples > Arduino OTA > BasicOTA.ino
+*********/
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+// Replace with your network credentials
+const char* ssid = "TPLINK";
+const char* password = "23Jan1990";
+
+WiFiManager wifiManager;
+WiFiServer server(80);// Set port to 80
+
+
+String header; // This storees the HTTP request
+
+const int ESP_BUILTIN_LED = 2;
+
+// Declare the pins to which the appliances are connected via relays
+int app1 = D1; //appliance 1
+int app2 = D2; //appliance 2`
+//you can add more more appliances below.
+
+String app1state = "off";// state of appliance1
+String app2state = "off";// state of appliance2
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  wifiManager.autoConnect("AutoConnectAP");
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  pinMode(ESP_BUILTIN_LED, OUTPUT);
+   pinMode(app1, OUTPUT);
+  pinMode(app2, OUTPUT);
+  digitalWrite(app1, LOW);
+  digitalWrite(app2, LOW);
+  server.begin();
+}
+
+void loop() {
+  ArduinoOTA.handle();
+  digitalWrite(ESP_BUILTIN_LED, LOW);
+  delay(1000);
+  digitalWrite(ESP_BUILTIN_LED, HIGH);
+  delay(1000);
+    WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /app1/on") >= 0) {
+              Serial.println("App 1 on");
+              app1state = "on";
+              digitalWrite(app1, HIGH);
+            } else if (header.indexOf("GET /app1/off") >= 0) {
+              Serial.println("App 1 off");
+              app1state = "off";
+              digitalWrite(app1, LOW);
+            } else if (header.indexOf("GET /app2/on") >= 0) {
+              Serial.println("App 2 on");
+              app2state = "on";
+              digitalWrite(app2, HIGH);
+            } else if (header.indexOf("GET /app2/off") >= 0) {
+              Serial.println("App 2 off");
+              app2state = "off";
+              digitalWrite(app2, LOW);
+            }
+       
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #77878A;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>ESP8266 Web Server</h1>");
+            
+            // Display current state, and ON/OFF buttons for GPIO 5  
+            client.println("<p>app1 - State " + app1state + "</p>");
+            // If Appliance 1 is off, it displays the ON button       
+            if (app1state == "off") {
+              client.println("<p><a href=\"/app1/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/app1/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+               
+            // Display current state, and ON/OFF buttons for GPIO 4  
+            client.println("<p>app2 - State " + app2state + "</p>");
+            // If Appliance 2 is off, it displays the ON button       
+            if (app2state == "off") {
+              client.println("<p><a href=\"/app2/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/app2/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}
